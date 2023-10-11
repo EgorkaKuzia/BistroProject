50d405ec7ae9d0ceaa20626aafb59741ab3b4887
EgorkaKuzia <146088137+EgorkaKuzia@users.noreply.github.com>
2023-10-11 16:18:45 +0500

    Update Distant710.py

--- Distant710.py
+++ Distant710.py
@@ -1,1 +1,24 @@
-/
+def multiply(a, b):
+    i = a * b
+    return i
+multiply(2,3)
+
+#2
+
+def positive_sum(arr):
+    return sum(i for i in arr if i > 0)
+
+#3
+
+def make_negative( number ):
+    return -abs(number)
+
+#4
+
+def solution(string):
+   i = ''.join(reversed(string))
+   return i 
+
+#5
+
+solution('hello')
